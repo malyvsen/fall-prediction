bcf88ecd50a82249259cf89a1824eb7e4a9af538
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-08-05 13:58:54 +0200

    add data inspection utilities

---
+++ data/voltage_data.py
@@ -0,0 +1,54 @@
+import csv
+import numpy as np
+from scipy.signal import savgol_filter, find_peaks
+import matplotlib.pyplot as plt
+
+
+
+class VoltageData:
+    def __init__(self, resolution, voltage):
+        self.resolution = resolution
+        self.voltage = voltage
+
+
+    @classmethod
+    def from_file(cls, file):
+        reader = csv.reader(file)
+        first_row = next(reader)
+        second_row = next(reader)
+        voltage = []
+        for row in reader:
+            voltage.append(float(row[1]))
+        return cls(resolution=float(second_row[3]), voltage=np.array(voltage))
+
+
+    def smooth(self, window_seconds=0.25):
+        window_size = int(window_seconds // self.resolution // 2 * 2) + 1
+        smooth_voltage = savgol_filter(self.voltage, window_size, polyorder=3)
+        return type(self)(self.resolution, smooth_voltage)
+
+
+    def change_resolution(self, target_resolution):
+        resolution_ratio = int(target_resolution // self.resolution)
+        voltage_cut = self.voltage[:len(self.voltage) // resolution_ratio * resolution_ratio]
+        result_voltage = np.mean(np.reshape(voltage_cut, (-1, resolution_ratio)), axis=-1)
+        return type(self)(target_resolution, result_voltage)
+
+
+    def power_spectrum(self):
+        return np.abs(np.fft.rfft(self.voltage))
+
+
+    def peaks(self, prominence=None, distance_seconds=0.3):
+        if prominence is None:
+            prominence = 0.5 * (np.max(self.voltage) - np.min(self.voltage))
+        distance = int(distance_seconds // self.resolution)
+        return find_peaks(self.voltage, prominence=prominence, distance=distance)[0]
+
+
+    def plot(self, window_title='VoltageData plot'):
+        plt.plot(self.voltage)
+        plt.plot(self.peaks(), self.voltage[self.peaks()], 'x')
+        plt.ylabel('voltage')
+        plt.gcf().canvas.set_window_title(window_title)
+        plt.show()
